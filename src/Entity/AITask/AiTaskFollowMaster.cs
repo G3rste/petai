789e4bea83f75f904d4d776b8cf5fbf13d22558e
G3rste <philippgerstenberg@web.de>
2022-02-05 00:08:30 +0100

    extend base game followmaster instead of creating new one

--- src/Entity/AITask/AiTaskFollowMaster.cs
+++ src/Entity/AITask/AiTaskFollowMaster.cs
@@ -1,18 +1,12 @@
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
 
 namespace PetAI
 {
-    public class AiTaskFollowMaster : AiTaskBase
+    public class AiTaskFollowMaster : AiTaskStayCloseToGuardedEntity 
     {
-
-        Entity targetEntity;
-        float moveSpeed = 0.03f;
-        float range = 20f;
-        float maxDistance = 5f;
-        bool stuck = false;
-
         string commandName = "followmaster";
         public AiTaskFollowMaster(EntityAgent entity) : base(entity)
         {
@@ -21,21 +15,6 @@ namespace PetAI
         public override void LoadConfig(JsonObject taskConfig, JsonObject aiConfig)
         {
             base.LoadConfig(taskConfig, aiConfig);
-
-            if (taskConfig["movespeed"] != null)
-            {
-                moveSpeed = taskConfig["movespeed"].AsFloat(0.03f);
-            }
-
-            if (taskConfig["searchrange"] != null)
-            {
-                range = taskConfig["searchrange"].AsFloat(20f);
-            }
-
-            if (taskConfig["maxdistance"] != null)
-            {
-                maxDistance = taskConfig["maxdistance"].AsFloat(5f);
-            }
 
             if (taskConfig["command"] != null)
             {
@@ -52,44 +31,6 @@ namespace PetAI
             }
             return entity.GetBehavior<EntityBehaviorReceiveCommand>().complexCommand == commandName
                 && targetEntity.ServerPos.SquareDistanceTo(entity.ServerPos.X, entity.ServerPos.Y, entity.ServerPos.Z) > maxDistance * maxDistance;
-        }
-        public override void StartExecute()
-        {
-            base.StartExecute();
-
-            float size = targetEntity.CollisionBox.XSize;
-            pathTraverser.NavigateTo(targetEntity.ServerPos.XYZ, moveSpeed, size + 0.2f, OnGoalReached, OnStuck, false, 1000, true);
-
-            stuck = false;
-        }
-
-        public override bool ContinueExecute(float dt)
-        {
-
-            double x = targetEntity.ServerPos.X;
-            double y = targetEntity.ServerPos.Y;
-            double z = targetEntity.ServerPos.Z;
-
-            pathTraverser.CurrentTarget.X = x;
-            pathTraverser.CurrentTarget.Y = y;
-            pathTraverser.CurrentTarget.Z = z;
-
-            if (entity.ServerPos.SquareDistanceTo(x, y, z) < maxDistance * maxDistance / 4)
-            {
-                pathTraverser.Stop();
-                return false;
-            }
-
-            return targetEntity.Alive && !stuck && pathTraverser.Active;
-        }
-
-        private void OnStuck()
-        {
-            stuck = true;
-        }
-
-        private void OnGoalReached()
-        {
         }
     }
 }
