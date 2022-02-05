bdada9a6416f5246480f1b1a86f77285b1d26174
G3rste <philippgerstenberg@web.de>
2022-02-05 17:32:33 +0100

    Attempt to fix Nullpointerexception when teleporting

--- src/Entity/AITask/AiTaskFollowMaster.cs
+++ src/Entity/AITask/AiTaskFollowMaster.cs
@@ -1,11 +1,11 @@
 using Vintagestory.API.Common;
-using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 
 namespace PetAI
 {
-    public class AiTaskFollowMaster : AiTaskStayCloseToGuardedEntity 
+    public class AiTaskFollowMaster : AiTaskStayCloseToEntity
     {
         string commandName = "followmaster";
         public AiTaskFollowMaster(EntityAgent entity) : base(entity)
@@ -24,13 +24,18 @@ namespace PetAI
 
         public override bool ShouldExecute()
         {
-            if (targetEntity == null || !targetEntity.Alive || targetEntity.ShouldDespawn)
+            if (targetEntity == null || !targetEntity.Alive || targetEntity.ShouldDespawn || !targetEntity.IsInteractable)
             {
                 targetEntity = entity?.GetBehavior<EntityBehaviorTameable>()?.owner?.Entity;
                 return false;
             }
             return entity.GetBehavior<EntityBehaviorReceiveCommand>().complexCommand == commandName
                 && targetEntity.ServerPos.SquareDistanceTo(entity.ServerPos.X, entity.ServerPos.Y, entity.ServerPos.Z) > maxDistance * maxDistance;
+        }
+
+        public override void OnNoPath(Vec3d target)
+        {
+            // ignore
         }
     }
 }
