1fcef6b6aca00a132fa838046f59f7d9453ca127
G3rste <philippgerstenberg@web.de>
2022-05-29 15:56:13 +0200

    fix release script

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=$(grep -o '"petai":\s*"\([0-9]\.*\)*"' resources/modinfo.json | grep -o '\([0-9]\.*\)*')
+version=$(grep -o '"version":\s*"\([0-9]\.*\)*"' resources/modinfo.json | grep -o '\([0-9]\.*\)*')
 releasefile='bin/petai_v'$version'.zip'
 dotnet build -c release
 mv bin/petai.zip $releasefile
