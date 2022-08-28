99425691cdd9f0d82fa1a903c21723754cca2291
G3rste <philippgerstenberg@web.de>
2022-08-28 11:33:16 +0200

    remove limited uses from reskinner

--- src/Item/ItemTextureRotator.cs
+++ src/Item/ItemTextureRotator.cs
@@ -24,23 +24,8 @@ namespace PetAI
                 textureMorpher.WatchedAttributes.MarkPathDirty("textureIndex");
                 sapi.World.SpawnEntity(PetUtil.EntityFromTree(PetUtil.EntityToTree(textureMorpher), sapi.World));
                 textureMorpher.Die(EnumDespawnReason.Removed);
-
-                var usagesleft = slot.Itemstack.Attributes.GetInt("usages", 5) - 1;
-                slot.Itemstack.Attributes.SetInt("usages", usagesleft);
-                if (usagesleft == 0)
-                {
-                    slot.TakeOut(1);
-                    slot.Itemstack?.Attributes?.SetInt("usages", 5);
-                }
-                slot.MarkDirty();
             }
             handling = EnumHandHandling.Handled;
-        }
-
-        public override void GetHeldItemInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo)
-        {
-            base.GetHeldItemInfo(inSlot, dsc, world, withDebugInfo);
-            dsc.Append(Lang.Get("petai:desc-texturerotator", inSlot.Itemstack.Attributes.GetInt("usages", 5)));
         }
     }
 }
