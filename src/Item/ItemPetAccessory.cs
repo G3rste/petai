0aaf55beac1b10cd11a8d394a5c95d02936625b7
G3rste <philippgerstenberg@web.de>
2023-04-05 20:21:39 +0200

    Add customizable shape per entity

--- src/Item/ItemPetAccessory.cs
+++ src/Item/ItemPetAccessory.cs
@@ -1,11 +1,10 @@
 using System;
-using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Util;
 
 namespace PetAI
 {
-    public class ItemPetAccessory : Item
+    public class ItemPetAccessory : Item, IWearableShapeSupplier
     {
         public PetAccessoryType type
         {
@@ -24,6 +23,16 @@ namespace PetAI
         }
         public int backpackSlots => Attributes["backpackslots"].AsInt(0);
         public bool canBeWornBy(string pet) => Attributes["validPets"].AsArray<string>(new string[0]).Contains(pet);
+
+        public Shape GetShape(ItemStack stack, EntityAgent forEntity)
+        {
+            if(Attributes["shapes"][forEntity.Code.Path].Exists){
+                var compositeShape = Attributes["shapes"][forEntity.Code.Path].AsObject<CompositeShape>();
+                var shapePath = compositeShape.Base.CopyWithPath("shapes/" + compositeShape.Base.Path + ".json");
+                return Vintagestory.API.Common.Shape.TryGet(forEntity.Api, shapePath);
+            }
+            return null;
+        }
 
         public float damageReduction => Attributes["damageReduction"].AsFloat(0);
     }
