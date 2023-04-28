b442aee70a8cfc4c0e6558f6fcbe1ec3af24843a
G3rste <philippgerstenberg@web.de>
2023-04-28 18:39:35 +0200

    Teleport precisely when to far away from the player, but on no other occasions

--- src/Entity/AITask/AiTaskFollowMaster.cs
+++ src/Entity/AITask/AiTaskFollowMaster.cs
@@ -33,6 +33,56 @@ namespace PetAI
                 && targetEntity.ServerPos.SquareDistanceTo(entity.ServerPos.X, entity.ServerPos.Y, entity.ServerPos.Z) > maxDistance * maxDistance;
         }
 
+        //overridden base method to avoid constant teleporting when stuck
+        public override void StartExecute()
+        {
+            base.StartExecute();
+
+            float size = targetEntity.SelectionBox.XSize;
+
+            pathTraverser.NavigateTo_Async(targetEntity.ServerPos.XYZ, moveSpeed, size + 0.2f, OnGoalReached, () => stuck = true, null, 1000, 1);
+
+            targetOffset.Set(entity.World.Rand.NextDouble() * 2 - 1, 0, entity.World.Rand.NextDouble() * 2 - 1);
+
+            stuck = false;
+
+            double x = targetEntity.ServerPos.X + targetOffset.X;
+            double y = targetEntity.ServerPos.Y;
+            double z = targetEntity.ServerPos.Z + targetOffset.Z;
+
+            float dist = entity.ServerPos.SquareDistanceTo(x, y, z);
+
+            if (allowTeleport && dist > teleportAfterRange * teleportAfterRange)
+            {
+                tryTeleport();
+            }
+        }
+
+        public override bool ContinueExecute(float dt)
+        {
+            double x = targetEntity.ServerPos.X + targetOffset.X;
+            double y = targetEntity.ServerPos.Y;
+            double z = targetEntity.ServerPos.Z + targetOffset.Z;
+
+            pathTraverser.CurrentTarget.X = x;
+            pathTraverser.CurrentTarget.Y = y;
+            pathTraverser.CurrentTarget.Z = z;
+
+            float dist = entity.ServerPos.SquareDistanceTo(x, y, z);
+
+            if (dist < 3 * 3)
+            {
+                pathTraverser.Stop();
+                return false;
+            }
+
+            if (allowTeleport && dist > teleportAfterRange * teleportAfterRange)
+            {
+                tryTeleport();
+            }
+
+            return !stuck && pathTraverser.Active;
+        }
         public override void OnNoPath(Vec3d target)
         {
             // ignore
