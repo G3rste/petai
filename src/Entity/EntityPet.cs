9dbb7119314b89a8d2c4ce081472c1b03f206a39
G3rste <philippgerstenberg@web.de>
2023-11-15 22:23:52 +0100

    something else with Shaperendering broke in vanilla

--- src/Entity/EntityPet.cs
+++ src/Entity/EntityPet.cs
@@ -5,6 +5,8 @@ using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Datastructures;
 using System.IO;
 using Vintagestory.GameContent;
+using System.Collections.Generic;
+using Vintagestory.API.Client;
 
 namespace PetAI
 {
@@ -128,6 +130,80 @@ namespace PetAI
                 return false;
             }
             return base.ShouldReceiveDamage(damageSource, damage);
+        }
+
+        protected override Shape addGearToShape(ItemSlot slot, Shape entityShape, string shapePathForLogging)
+        {
+            if (slot.Empty) return entityShape;
+            ItemStack stack = slot.Itemstack;
+            JsonObject attrObj = stack.Collectible.Attributes;
+
+            float damageEffect = 0;
+            if (stack.ItemAttributes?["visibleDamageEffect"].AsBool() == true)
+            {
+                damageEffect = Math.Max(0, 1 - (float)stack.Collectible.GetRemainingDurability(stack) / stack.Collectible.GetMaxDurability(stack) * 1.1f);
+            }
+
+            string[] disableElements = attrObj?["disableElements"]?.AsArray<string>(null);
+            if (disableElements != null)
+            {
+                foreach (var val in disableElements)
+                {
+                    entityShape.RemoveElementByName(val);
+                }
+            }
+
+            if (attrObj?["wearableAttachment"].Exists != true) return entityShape;
+
+            Shape gearShape=null;
+            AssetLocation shapePath;
+            CompositeShape compGearShape = null;
+            if (stack.Collectible is IWearableShapeSupplier iwss)
+            {
+                gearShape = iwss.GetShape(stack, this);
+            }
+
+            if (gearShape == null) {
+                compGearShape = !attrObj["attachShape"].Exists ? (stack.Class == EnumItemClass.Item ? stack.Item.Shape : stack.Block.Shape) : attrObj["attachShape"].AsObject<CompositeShape>(null, stack.Collectible.Code.Domain);
+                shapePath = compGearShape.Base.CopyWithPath("shapes/" + compGearShape.Base.Path + ".json");
+                gearShape = Shape.TryGet(Api, shapePath);
+                if (gearShape == null)
+                {
+                    Api.World.Logger.Warning("Entity armor shape {0} defined in {1} {2} not found or errored, was supposed to be at {3}. Armor piece will be invisible.", compGearShape.Base, stack.Class, stack.Collectible.Code, shapePath);
+                    return null;
+                }
+            }
+
+            string texturePrefixCode = stack.Collectible.Code.ToShortString();
+
+            // Item stack textures take precedence over shape textures
+            if (gearShape.Textures == null) gearShape.Textures = new Dictionary<string, AssetLocation>();
+            var collectibleDict = stack.Class == EnumItemClass.Block ? stack.Block.Textures : stack.Item.Textures;
+            foreach (var val in collectibleDict)
+            {
+                gearShape.Textures[val.Key] = val.Value.Base;
+            }
+
+            var textures = Properties.Client.Textures;
+            Api.Logger.Debug("Doing the thing!");
+            entityShape.StepParentShape(
+                gearShape, 
+                texturePrefixCode, 
+                compGearShape?.Base.ToString() ?? "Custom texture from ItemWearableShapeSupplier " + string.Format("defined in {0} {1}", stack.Class, stack.Collectible.Code),
+                shapePathForLogging, 
+                Api.World.Logger,
+                (texcode, tloc) =>
+                {
+                    var cmpt = textures[texturePrefixCode + "-" + texcode] = new CompositeTexture(tloc);
+                    cmpt.Bake(Api.Assets);
+                    (Api as ICoreClientAPI).EntityTextureAtlas.GetOrInsertTexture(cmpt.Baked.TextureFilenames[0], out int textureSubid, out _);
+                    cmpt.Baked.TextureSubId = textureSubid;
+                },
+                damageEffect
+            );
+
+
+            return entityShape;
         }
     }
 }
--- src/Entity/EntityPlayerFix.cs
+++ src/Entity/EntityPlayerFix.cs
@@ -36,18 +36,17 @@ namespace PetAI
 
             if (attrObj?["wearableAttachment"].Exists != true) return entityShape;
 
-            Shape gearShape = null;
-            AssetLocation shapePath = null;
+            Shape gearShape=null;
+            AssetLocation shapePath;
             CompositeShape compGearShape = null;
             if (stack.Collectible is IWearableShapeSupplier iwss)
             {
                 gearShape = iwss.GetShape(stack, this);
             }
 
-            if (gearShape == null)
-            {
+            if (gearShape == null) {
                 compGearShape = !attrObj["attachShape"].Exists ? (stack.Class == EnumItemClass.Item ? stack.Item.Shape : stack.Block.Shape) : attrObj["attachShape"].AsObject<CompositeShape>(null, stack.Collectible.Code.Domain);
-                shapePath = shapePath = compGearShape.Base.CopyWithPath("shapes/" + compGearShape.Base.Path + ".json");
+                shapePath = compGearShape.Base.CopyWithPath("shapes/" + compGearShape.Base.Path + ".json");
                 gearShape = Shape.TryGet(Api, shapePath);
                 if (gearShape == null)
                 {
@@ -56,171 +55,36 @@ namespace PetAI
                 }
             }
 
+            string texturePrefixCode = stack.Collectible.Code.ToShortString();
 
-            bool added = false;
-            foreach (var val in gearShape.Elements)
+            // Item stack textures take precedence over shape textures
+            if (gearShape.Textures == null) gearShape.Textures = new Dictionary<string, AssetLocation>();
+            var collectibleDict = stack.Class == EnumItemClass.Block ? stack.Block.Textures : stack.Item.Textures;
+            foreach (var val in collectibleDict)
             {
-                ShapeElement elem;
-
-                if (val.StepParentName != null)
-                {
-                    elem = entityShape.GetElementByName(val.StepParentName, StringComparison.InvariantCultureIgnoreCase);
-                    if (elem == null)
-                    {
-                        Api.World.Logger.Warning("Entity gear shape {0} defined in {1} {2} requires step parent element with name {3}, but no such element was found in shape {4}. Will not be visible.", compGearShape.Base, slot.Itemstack.Class, slot.Itemstack.Collectible.Code, val.StepParentName, shapePathForLogging);
-                        continue;
-                    }
-                }
-                else
-                {
-                    Api.World.Logger.Warning("Entity gear shape element {0} in shape {1} defined in {2} {3} did not define a step parent element. Will not be visible.", val.Name, compGearShape.Base, slot.Itemstack.Class, slot.Itemstack.Collectible.Code);
-                    continue;
-                }
-
-                if (elem.Children == null)
-                {
-                    elem.Children = new ShapeElement[] { val };
-                }
-                else
-                {
-                    elem.Children = (ShapeElement[])elem.Children.Append(val);
-                }
-
-                val.ParentElement = elem;
-
-                val.WalkRecursive((el) =>
-                {
-                    el.DamageEffect = damageEffect;
-
-                    foreach (var face in el.FacesResolved)
-                    {
-                        if (face != null) face.Texture = stack.Collectible.Code + "-" + face.Texture;
-                    }
-                });
-
-                added = true;
+                gearShape.Textures[val.Key] = val.Value.Base;
             }
 
-            if (gearShape.Animations != null)
-            {
-                foreach (var gearAnim in gearShape.Animations)
+            var textures = Properties.Client.Textures;
+            Api.Logger.Debug("Doing the thing!");
+            entityShape.StepParentShape(
+                gearShape, 
+                texturePrefixCode, 
+                compGearShape?.Base.ToString() ?? "Custom texture from ItemWearableShapeSupplier " + string.Format("defined in {0} {1}", stack.Class, stack.Collectible.Code),
+                shapePathForLogging, 
+                Api.World.Logger,
+                (texcode, tloc) =>
                 {
-                    var entityAnim = entityShape.Animations.FirstOrDefault(anim => anim.Code == gearAnim.Code);
-                    if (entityAnim != null)
-                    {
-                        for (int gi = 0; gi < gearAnim.KeyFrames.Length; gi++)
-                        {
-                            var gearKeyFrame = gearAnim.KeyFrames[gi];
-                            var entityKeyFrame = getOrCreateKeyFrame(entityAnim, gearKeyFrame.Frame);
+                    var cmpt = textures[texturePrefixCode + "-" + texcode] = new CompositeTexture(tloc);
+                    cmpt.Bake(Api.Assets);
+                    (Api as ICoreClientAPI).EntityTextureAtlas.GetOrInsertTexture(cmpt.Baked.TextureFilenames[0], out int textureSubid, out _);
+                    cmpt.Baked.TextureSubId = textureSubid;
+                },
+                damageEffect
+            );
 
-                            foreach (var val in gearKeyFrame.Elements)
-                            {
-                                entityKeyFrame.Elements[val.Key] = val.Value;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (added && gearShape.Textures != null)
-            {
-                Dictionary<string, AssetLocation> newdict = new Dictionary<string, AssetLocation>();
-                foreach (var val in gearShape.Textures)
-                {
-                    newdict[stack.Collectible.Code + "-" + val.Key] = val.Value;
-                }
-
-                // Item overrides
-                var collDict = stack.Class == EnumItemClass.Block ? stack.Block.Textures : stack.Item.Textures;
-                foreach (var val in collDict)
-                {
-                    newdict[stack.Collectible.Code + "-" + val.Key] = val.Value.Base;
-                }
-
-                gearShape.Textures = newdict;
-
-                foreach (var val in gearShape.Textures)
-                {
-                    CompositeTexture ctex = new CompositeTexture() { Base = val.Value };
-
-                    entityShape.TextureSizes[val.Key] = new int[] { gearShape.TextureWidth, gearShape.TextureHeight };
-
-                    AssetLocation armorTexLoc = val.Value;
-
-                    // Weird backreference to the shaperenderer. Should be refactored.
-                    var texturesByLoc = extraTextureByLocation;
-                    var texturesByName = extraTexturesByTextureName;
-
-                    BakedCompositeTexture bakedCtex;
-
-                    ICoreClientAPI capi = Api as ICoreClientAPI;
-
-                    if (!texturesByLoc.TryGetValue(armorTexLoc, out bakedCtex))
-                    {
-                        int textureSubId = 0;
-                        TextureAtlasPosition texpos;
-
-                        capi.EntityTextureAtlas.GetOrInsertTexture(armorTexLoc, out textureSubId, out texpos, () =>
-                        {
-                            IAsset texAsset = Api.Assets.TryGet(armorTexLoc.Clone().WithPathPrefixOnce("textures/").WithPathAppendixOnce(".png"));
-                            if (texAsset != null)
-                            {
-                                return texAsset.ToBitmap(capi);
-                            }
-
-                            capi.World.Logger.Warning("Entity armor shape {0} defined texture {1}, no such texture found.", shapePath, armorTexLoc);
-                            return null;
-                        });
-
-                        ctex.Baked = new BakedCompositeTexture() { BakedName = armorTexLoc, TextureSubId = textureSubId };
-
-                        texturesByName[val.Key] = ctex;
-                        texturesByLoc[armorTexLoc] = ctex.Baked;
-                    }
-                    else
-                    {
-                        ctex.Baked = bakedCtex;
-                        texturesByName[val.Key] = ctex;
-                    }
-                }
-
-                foreach (var val in gearShape.TextureSizes)
-                {
-                    // fixhere start
-                    entityShape.TextureSizes[stack.Collectible.Code + "-" + val.Key] = val.Value;
-                    // fixhere end
-                }
-            }
-            //foreach(var val in entityShape.TextureSizes)
-            //    Api.Logger.Debug("texturesize for " + val.Key + " = "+ val.Value[0] + ", " +val.Value[1]);
 
             return entityShape;
-        }
-        private AnimationKeyFrame getOrCreateKeyFrame(Animation entityAnim, int frame)
-        {
-            for (int ei = 0; ei < entityAnim.KeyFrames.Length; ei++)
-            {
-                var entityKeyFrame = entityAnim.KeyFrames[ei];
-                if (entityKeyFrame.Frame == frame)
-                {
-                    return entityKeyFrame;
-                }
-            }
-
-            for (int ei = 0; ei < entityAnim.KeyFrames.Length; ei++)
-            {
-                var entityKeyFrame = entityAnim.KeyFrames[ei];
-                if (entityKeyFrame.Frame > frame)
-                {
-                    var kfm = new AnimationKeyFrame() { Frame = frame };
-                    entityAnim.KeyFrames = entityAnim.KeyFrames.InsertAt(kfm, frame);
-                    return kfm;
-                }
-            }
-
-            var kf = new AnimationKeyFrame() { Frame = frame };
-            entityAnim.KeyFrames = entityAnim.KeyFrames.InsertAt(kf, 0);
-            return kf;
         }
     }
 }
