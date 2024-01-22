e4968c2fcade60630196ed8a6d68fcb0ebc767bd
G3rste <philippgerstenberg@web.de>
2024-01-23 00:48:31 +0100

    added select  popular modders script

---
+++ select-popular-modders.ps1
@@ -0,0 +1,8 @@
+$webData = Invoke-WebRequest -Uri "https://mods.vintagestory.at/api/mods"; 
+$jsonData = ConvertFrom-Json $webData.content; 
+$jsonData.mods | Select-Object author, downloads | Group-Object -Property author | %{
+    New-Object psobject -Property @{
+        author = $_.Name
+        Downloads = ($_.Group | Measure-Object downloads -Sum).Sum
+    }
+} | Sort-Object Downloads -Descending | foreach { $line =0 } { $line++; $_ } | ft @{ n = 'line'; e= { $line } }, author, Downloads
