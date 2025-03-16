84fa5f9c8fb6c3bcf19145068c7af534654b5af2
G3rste <philippgerstenberg@web.de>
2025-03-16 19:34:53 +0100

    why did I remove this?

--- src/Entity/Behavior/BehaviorTameable.cs
+++ src/Entity/Behavior/BehaviorTameable.cs
@@ -449,6 +449,33 @@ namespace PetAI
             }
         }
 
+        public override void OnEntityDeath(DamageSource damageSourceForDeath)
+        {
+            base.OnEntityDeath(damageSourceForDeath);
+            if (!string.IsNullOrEmpty(ownerId) && entity.Api is ICoreServerAPI sapi)
+            {
+                sapi.SendMessage(cachedOwner,
+                GlobalConstants.GeneralChatGroup,
+                Lang.Get("petai:message-pet-dead",
+                entity.GetBehavior<EntityBehaviorNameTag>()?.DisplayName),
+                EnumChatType.Notification);
+
+                if (entity.Api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.Find(ml => ml is WaypointMapLayer) is WaypointMapLayer waypointMap)
+                {
+                    waypointMap.AddWaypoint(new Waypoint()
+                    {
+                        Color = 9044739,
+                        Icon = "gravestone",
+                        Pinned = true,
+                        Position = entity.ServerPos.XYZ,
+                        OwningPlayerUid = ownerId,
+                        Title = Lang.Get("petai:message-pet-dead", entity.GetBehavior<EntityBehaviorNameTag>()?.DisplayName),
+                    },
+                        cachedOwner as IServerPlayer);
+                }
+            }
+        }
+
         public override void GetInfoText(StringBuilder infotext)
         {
             if (cachedOwner == null) return;
